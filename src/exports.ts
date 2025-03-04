2496322f15e0308a26c873050ec0c9b29e205e7c
Michael Dinerstein <md@missionmike.dev>
2025-03-04 00:04:35 -0800

    feat: update doc.

--- src/exports.ts
+++ src/exports.ts
@@ -1,3 +1,6 @@
+/**
+ * This file is used to export all the components, types, and utilities that are available in the library.
+ */
 export * from "@/types";
 
 export { groupSkillsForUserByYearExperience } from "@/util/structure";
