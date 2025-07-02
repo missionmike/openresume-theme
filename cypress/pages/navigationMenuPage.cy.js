4eced97bfbbb40ab0e48449980ea114f210c70ba
silentZaika <el.savinova@gmail.com>
2025-07-02 20:42:45 +0000

    Add page object model for navMenu and davidsTheme, add test for issue-40

---
+++ cypress/pages/navigationMenuPage.cy.js
@@ -0,0 +1,18 @@
+export class NavigationMenuPage {
+  getNavPrimaryMenu() {
+    cy.visit("/");
+    return cy.get("button[data-testid='nav-primary-menu-button']");
+  }
+
+  getDavidsThemeItem() {
+    return cy.get("a[href='/theme/davids']");
+  }
+
+  getClassicThemeItem() {
+    return cy.get("a[href='/theme/default']");
+  }
+
+  getCloseView() {
+    return cy.get("button[data-testid='nav-primary-menu-close-button']");
+  }
+}
